4a517df66ba047c103e4226a371dc414820794c0
Isaac Humberto de la Rosa Mendez <idelarosa@intelexion.com.mx>
2019-05-15 17:03:39 -0500

    tg
    tg

--- Examples/djangocrud/alumnos/views.py
+++ Examples/djangocrud/alumnos/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from alumnos import controller
-
+from .models import Alumnos
+from django.http import JsonResponse
 
 def index(request):
     _result = controller.GetAlumnos()
     return render(request, "alumnos/index.html", 
-    {'title': _result})
+    {'alumnos': _result})
