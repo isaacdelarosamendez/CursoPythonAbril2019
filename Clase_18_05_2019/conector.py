6eb7ee7cd491a0f2e69230e90668915426870203
Isaac Humberto de la Rosa Mendez <idelarosa@intelexion.com.mx>
2019-05-20 16:39:13 -0500

    12
    12

--- Clase_18_05_2019/conector.py
+++ Clase_18_05_2019/conector.py
@@ -1,1 +1,23 @@
 import psycopg2
+
+class Helper():
+    def GetConnection(self):
+        #Creamos la conexion
+        connection = psycopg2.connect(host="localhost",
+        dbname="uanl", 
+        user="postgres", 
+        password="123456")
+        return connection
+    
+    def GetCursor(self,con):
+        return con.cursor()
+    
+    def Execute(self,query):
+        cursor = self.GetCursor(self.GetConnection())
+        cursor.execute(query)
+        return cursor.fetchall()
+
+_helper = Helper()
+print(_helper.Execute("select * from cursos"))
+
+
